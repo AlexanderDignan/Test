75323671b6c349d5b4d06c18b3ed8d0b9e2fae5b
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-11 10:42:32 -0400

    test new method

--- backend.tf
+++ backend.tf
@@ -1,10 +1,7 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-eastus"  
-    storage_account_name = "cs21003200129b14f66"
-    container_name       = "tfstate"                       
-    key                  = "prod.terraform.tfstate"
-    use_azuread_auth = true
+  backend "azurerm" {               
+    key      = "prod.terraform.tfstate"
+    use_oidc = true
   }
 
   required_providers {
