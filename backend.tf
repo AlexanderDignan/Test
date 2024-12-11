6f5a32a3c8e38ccb5637e3bcbe37a173dba5f26b
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-11 10:14:32 -0400

    try odic

--- backend.tf
+++ backend.tf
@@ -17,9 +17,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id = "3c61b2f3-9809-4b4e-b7de-b89836b7bb73"
-  client_secret = "DHm8Q~WrgvieEN9CkTvAxcHj4k1lwTD9AiQJEcpk"
-  tenant_id = "4b497271-d1e9-49b7-bea2-070105eed6ab"
-  subscription_id = "bc01f5a7-8f79-4a1e-99df-29b855e8d965"
+  use_oidc = true
 }
