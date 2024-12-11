8953195a39d43af85b8d96584c6df0075e899532
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-11 10:06:21 -0400

    fix: backend

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-eastus"  
+    storage_account_name = "cs21003200129b14f66"
+    container_name       = "tfstate"                       
+    key                  = "prod.terraform.tfstate"
     use_azuread_auth = true
   }
 
--- testing.tf
+++ testing.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "example" {
-  name     = "exampl"
+  name     = "example"
   location = "West Europe"
 }
