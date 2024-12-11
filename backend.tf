14240c133f768f4a2830cbc2591973689f118218
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-11 11:42:49 -0400

    hardcode backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "azurerm" {               
     key      = "prod.terraform.tfstate"
+    resource_group_name = "cloud-shell-storage-eastus"
+    storage_account_name = "cs21003200129b14f66"
+    container_name = "tfstate"
     use_oidc = true
   }
 
