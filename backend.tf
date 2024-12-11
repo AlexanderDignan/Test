4312cc7a1cb46b5f47d19f1591c8fed3476bbb96
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-11 11:44:42 -0400

    test

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     resource_group_name = "cloud-shell-storage-eastus"
     storage_account_name = "cs21003200129b14f66"
     container_name = "tfstate"
+    access_key = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
     use_oidc = true
   }
 
