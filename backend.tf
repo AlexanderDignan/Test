06307d7036bf91d98da4aa90274909778271337b
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-12 15:51:56 -0400

    fix: backend

--- backend.tf
+++ backend.tf
@@ -5,9 +5,6 @@ terraform {
     resource_group_name  = "cloud-shell-storage-eastus"
     storage_account_name = "cs21003200129b14f66"
     container_name       = "tfstate"
-    # subscription_id      = "bc01f5a7-8f79-4a1e-99df-29b855e8d965"
-    # client_id            = "d3c1075f-353d-4f24-b056-8387f299c4f6"
-    # tenant_id            = "4b497271-d1e9-49b7-bea2-070105eed6ab"
     use_oidc             = true
   }
 
