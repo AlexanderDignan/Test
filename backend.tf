b615e18e05a8d202faf6c79af7113fd1fd876176
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-12 13:26:10 -0400

    port fixes

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
+  required_version = "~> 1.0"
   backend "azurerm" {
     key                  = "prod.terraform.tfstate"
     resource_group_name  = "cloud-shell-storage-eastus"
     storage_account_name = "cs21003200129b14f66"
     container_name       = "tfstate"
-    access_key           = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
     use_oidc             = true
   }
 
@@ -12,6 +12,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+
     }
   }
 }
