d815cddad6eb1a1a3d77fdcda0e9fdf391726305
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-12 09:45:21 -0400

    fmt for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "azurerm" {               
-    key      = "prod.terraform.tfstate"
-    resource_group_name = "cloud-shell-storage-eastus"
+  backend "azurerm" {
+    key                  = "prod.terraform.tfstate"
+    resource_group_name  = "cloud-shell-storage-eastus"
     storage_account_name = "cs21003200129b14f66"
-    container_name = "tfstate"
-    access_key = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
-    use_oidc = true
+    container_name       = "tfstate"
+    access_key           = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
+    use_oidc             = true
   }
 
   required_providers {
@@ -19,5 +19,5 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "bc01f5a7-8f79-4a1e-99df-29b855e8d965"
-  use_oidc = true
+  use_oidc        = true
 }
