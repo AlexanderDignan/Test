350fa514457835d74d5a92c7b4efbd4bff4ac73e
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-12 14:42:16 -0400

    remove environment from other stages comment out access key for now

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
     container_name       = "tfstate"
     client_id            = "d3c1075f-353d-4f24-b056-8387f299c4f6"
     tenant_id            = "4b497271-d1e9-49b7-bea2-070105eed6ab"
-    access_key           = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
+    #access_key           = "Pb1npJd494PbZxB69sOzCdZPpqG6BkD44R/UCWUs9AZGmvmHXgv80uaeSe0SnIEF7LkcTS+ZUOpN/z47u9b2UQ=="
     use_oidc             = true
   }
 
