c2c65c4186c786b25a130223287e90bd87d447e6
Alexander Dignan <131882673+AlexanderDignan-MRI@users.noreply.github.com>
2024-12-12 13:58:44 -0400

    test: hardcode clientid/tenantid

--- backend.tf
+++ backend.tf
@@ -5,6 +5,8 @@ terraform {
     resource_group_name  = "cloud-shell-storage-eastus"
     storage_account_name = "cs21003200129b14f66"
     container_name       = "tfstate"
+    client_id            = "d3c1075f-353d-4f24-b056-8387f299c4f6"
+    tenant_id            = "4b497271-d1e9-49b7-bea2-070105eed6ab"
     use_oidc             = true
   }
 
